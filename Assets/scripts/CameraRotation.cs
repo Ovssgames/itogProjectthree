10582e5afc4a64e5d2875cff5f7205c4fde72906
Ovssgames <fpulemetchik@mail.ru>
2024-02-08 23:02:58 +0300

    pre alpha 0.1

--- Assets/scripts/CameraRotation.cs
+++ Assets/scripts/CameraRotation.cs
@@ -4,15 +4,24 @@ using UnityEngine;
 
 public class CameraRotation : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    public float Speed;
+    public Transform CameraAxisTransform;
+    public float MinAngle;
+    public float MaxAngle;
 
-    // Update is called once per frame
-    void Update()
+
+
+    private void Update()
     {
-        
+        transform.localEulerAngles = new Vector3(0, transform.localEulerAngles.y + Time.deltaTime *  Speed * Input.GetAxis("Mouse X"), 0);
+
+        var NewAngleX = CameraAxisTransform.localEulerAngles.x + Time.deltaTime * Speed * Input.GetAxis("Mouse Y") * -1;
+        if (NewAngleX > 180)
+            NewAngleX -= 360;
+
+
+        NewAngleX = Mathf.Clamp(NewAngleX, MinAngle, MaxAngle);
+
+        CameraAxisTransform.localEulerAngles = new Vector3(NewAngleX, 0, 0);
     }
 }

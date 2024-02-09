e1c08208853fd5b852554794a3fb793b7d75d451
Ovssgames <fpulemetchik@mail.ru>
2024-02-09 23:26:28 +0300

    pre alphf 0.1.2

--- Assets/scripts/PlayerController.cs
+++ Assets/scripts/PlayerController.cs
@@ -4,11 +4,13 @@ using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-
     public float Gravity = 9.8f;
     public float JumpForce;
+    public float Speed;
 
     private float _fallVelocity = 0;
+    private Vector3 _moveVector;
+
     private CharacterController _CharacterController;
 
     private void Start()
@@ -18,22 +20,52 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+
+        _moveVector = Vector3.zero;
+        
         if (Input.GetKeyDown(KeyCode.Space) && _CharacterController.isGrounded)
         {
             _fallVelocity = -JumpForce;
-        } 
-            
-        
+        }
+
+
+        if (Input.GetKey(KeyCode.W))
+        {
+            _moveVector += transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.S))
+        {
+            _moveVector -= transform.forward;
+        }
+
+        if (Input.GetKey(KeyCode.D))
+        {
+            _moveVector += transform.right;
+        }
+
+        if (Input.GetKey(KeyCode.A))
+        {
+            _moveVector -= transform.right;
+        }
+
+
     }
 
     private void FixedUpdate()
     {
-        _CharacterController.Move(Vector3.forward * Time.fixedDeltaTime);
+        if (_CharacterController.isGrounded && _fallVelocity>0)
+        {
+            _fallVelocity = 0;
+        }
+
+
+        _CharacterController.Move(_moveVector * Time.fixedDeltaTime * Speed);
 
         _fallVelocity += Gravity * Time.fixedDeltaTime;
         _CharacterController.Move(Vector3.down * _fallVelocity * Time.fixedDeltaTime);
 
     }
 
-
+        
 }

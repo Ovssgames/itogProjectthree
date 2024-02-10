034889df84ccc74a11a9d45f07a3be9310c01c6b
Ovssgames <fpulemetchik@mail.ru>
2024-02-10 21:11:32 +0300

    pre alpha 0.1..3

---
+++ Assets/scripts/EnemyAI.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class EnemyAI : MonoBehaviour
+{
+    public List<Transform> PatrolPoint;
+
+    private NavMeshAgent _navMeshAgent;
+
+    private void Start()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
+
+        PickPatroolPoint();
+    }
+
+    private void Update()
+    {
+        if(_navMeshAgent.remainingDistance == 0)
+        {
+            PickPatroolPoint();
+        }
+    }
+
+    private void PickPatroolPoint()
+    {
+            _navMeshAgent.destination = PatrolPoint[Random.Range(0, PatrolPoint.Count)].position;
+    }
+}

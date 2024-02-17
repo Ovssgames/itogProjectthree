e7d1c470bd9953259e687840b0dfebb03477600d
Ovssgames <fpulemetchik@mail.ru>
2024-02-17 23:58:28 +0300

    pre alpha 0.2

--- Assets/scripts/EnemyAI.cs
+++ Assets/scripts/EnemyAI.cs
@@ -6,26 +6,64 @@ using UnityEngine.AI;
 public class EnemyAI : MonoBehaviour
 {
     public List<Transform> PatrolPoint;
+    public PlayerController Player;
+    public float vievAngle;
 
     private NavMeshAgent _navMeshAgent;
+    private bool _isPlayerNoticed;
 
     private void Start()
     {
-        _navMeshAgent = GetComponent<NavMeshAgent>();
-
+        NavGetComponent();
         PickPatroolPoint();
     }
 
     private void Update()
     {
-        if(_navMeshAgent.remainingDistance == 0)
-        {
-            PickPatroolPoint();
-        }
+        PatrolUp();
+        Raycast();
+    }
+    private void NavGetComponent()
+    {
+        _navMeshAgent = GetComponent<NavMeshAgent>();
     }
 
     private void PickPatroolPoint()
     {
             _navMeshAgent.destination = PatrolPoint[Random.Range(0, PatrolPoint.Count)].position;
+    }
+
+
+    private void PatrolUp()
+    {
+        if (_navMeshAgent.remainingDistance == 0 && !_isPlayerNoticed)
+        {
+            PickPatroolPoint();
+        }
+    }
+
+    private void Raycast()
+    {
+        var direction = Player.transform.position - transform.position;
+
+        _isPlayerNoticed = false;
+        if(Vector3.Angle(transform.forward, direction) < vievAngle)
+        {
+            RaycastHit hit;
+            if (Physics.Raycast(transform.position + Vector3.up, direction, out hit))
+            {
+                if (hit.collider.gameObject == Player.gameObject)
+                {
+                    _isPlayerNoticed = true;
+                }
+            }
+        }
+
+        if(_isPlayerNoticed)
+        {
+            _navMeshAgent.destination = Player.transform.position;
+        }
+        
+
     }
 }

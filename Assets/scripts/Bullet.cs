e3c977689074284778ced7470e8de3a7f11b97fa
Ovssgames <fpulemetchik@mail.ru>
2024-02-18 16:33:33 +0300

    Revert "Revert "pre alpha 0.2.1""
    This reverts commit 53904a68cd04c0e177230382938dfa9dae3b5a60.

---
+++ Assets/scripts/Bullet.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Bullet : MonoBehaviour
+{
+    public float speed;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void FixedUpdate()
+    {
+        BulletFixedUpdate();
+    }
+
+    private void BulletFixedUpdate()
+    {
+        transform.Translate(Vector3.forward * speed * Time.fixedDeltaTime);     //движение пули по оси z
+    }
+
+    private void OnCollisionEnter(Collision collision)
+    {
+
+    }
+
+    private void DestroyBullet()
+    {
+        Destroy(gameObject);
+    }
+}

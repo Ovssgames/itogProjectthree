310eb241451b335bb8d710909613c5145599734c
Ovssgames <fpulemetchik@mail.ru>
2024-03-13 23:13:03 +0300

    pre alpha 0.3.5

---
+++ Assets/scripts/Grenade/Explosion.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Explosion : MonoBehaviour
+{
+    public float maxSize = 5;
+    public float speed = 2;
+
+    private Vector3 _maxVector3Size;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        transform.localScale = Vector3.zero;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        transform.localScale += Vector3.one * Time.deltaTime * speed;
+        if(transform.localScale.z > maxSize)
+        {
+            Destroy(gameObject);
+        }
+    }
+}
---
+++ Assets/scripts/Grenade/Grenade.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Grenade : MonoBehaviour
+{
+    public float delay = 3;
+    public GameObject explosionprefab;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        Invoke("Explosion", delay);
+    }
+
+    private void Explosion()
+    {
+        Destroy(gameObject);
+        var explosion = Instantiate(explosionprefab);
+        explosion.transform.position = transform.position;
+    }
+}
